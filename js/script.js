ccc8283071888e111f82f0b7adc00d9865764979
Karol Dąbrowski <dabrowski.carl@gmail.com>
2018-04-07 02:25:44 +0200

    Add scripts and styling

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(function () {
+    var prefix = "https://cors-anywhere.herokuapp.com/";
+    var tweetLink = "https://twitter.com/intent/tweet?text=";
+    var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+    //get quote function
+    function getQuote() {
+        $.getJSON(prefix + quoteUrl, createTweet);
+        $.ajaxSetup({
+            cache: false
+        });
+    }
+    //create twit function 
+    function createTweet(input) {
+        var data = input[0];
+        var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+        var quoteText = $(data.content).text().trim();
+        var quoteAuthor = data.title;
+
+        if (!quoteAuthor.length) {
+            quoteAuthor = "Unknown author";
+        }
+        if (tweetText.length > 140) {
+            getQuote();
+        } else {
+            var tweet = tweetLink + encodeURIComponent(tweetText);
+            $('.quote').text(quoteText);
+            $('.author').text("Author: " + quoteAuthor);
+            $('.tweet').attr('href', tweet);
+        }
+    }
+    getQuote();
+    $('.trigger').click(function () {
+        getQuote();
+    });
+});
